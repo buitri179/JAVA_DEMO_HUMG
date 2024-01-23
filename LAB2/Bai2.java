db7acc15afea3fde8cae6228b3ebbdc807c316ea
buitri179 <buiductri179@gmail.com>
2024-01-23 19:22:07 +0700

    commit note

--- LAB2/Bai2.java
+++ LAB2/Bai2.java
@@ -3,12 +3,24 @@ import java.util.Scanner;
 
 public class Bai2 {
     public static void main(String[] args){
-        Scanner duc = new Scanner(System.in);
-        Double x,y;
-        System.out.print("so thu 1: ");
-        x = duc.nextDouble();
-        System.out.print("so thu 2: ");
-        y = duc.nextDouble();
+        Scanner cs = new Scanner(System.in);
+        double heso1, heso2;
+        System.out.print("he so 1: ");
+        heso1 = cs.nextDouble();
+        System.out.print("he so 2: ");
+        heso2 = cs.nextDouble();
+        if( heso1 % 2 == 0 && heso2 % 2 == 0 ){
+            System.out.println("he so 1 va he so 2 deu la so chan");
+        }
+        else if( heso1 % 2 != 0 && heso2 % 2 != 0){
+            System.out.println("he so 1 va he so 2 deu la so le");
+        }
+        else if( heso1 % 2 != 0 && heso2 % 2 == 0){
+            System.out.println("he so 1 la so le con he so 2 la so chan");
+        }
+        else{
+            System.out.println("he so 1 la so chan con he so 2 la so le");
+        }
 
     }
 }
