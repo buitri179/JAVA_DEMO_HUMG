1cd2f1e53cacd5c031446c0fc191af942beb52fe
buitri179 <buiductri179@gmail.com>
2024-01-24 09:19:39 +0700

    commit note

--- BaiThucHanhJava/GiaiPhuongTrinhBacHai.java
+++ BaiThucHanhJava/GiaiPhuongTrinhBacHai.java
@@ -1,5 +1,10 @@
 package BaiThucHanhJava;
 
+import java.util.Scanner;
+
 public class GiaiPhuongTrinhBacHai {
-    
+    public static void main(String[] args) {
+        Scanner dt =  new Scanner(System.in);
+        
+    }
 }
--- BaiThucHanhJava/GiaiPhuongTrinhBacNhat.java
+++ BaiThucHanhJava/GiaiPhuongTrinhBacNhat.java
@@ -1,5 +1,26 @@
 package BaiThucHanhJava;
 
+import java.util.Scanner;
+
 public class GiaiPhuongTrinhBacNhat {
     
+    public static void main(String[] args) {
+        Scanner dt =  new Scanner(System.in);
+    double a,b,x;
+    System.out.print("Nhap he so a: ");
+    a = dt.nextDouble();
+    System.out.print("Nhap so b: ");
+    b = dt.nextDouble();
+    if(b==0 && a==0){
+        System.out.println("He phuong trinh co vo so nghiem");
+    }
+    else if(a==0 && b!=0){
+        System.out.println("He phuong trinh vo nghiem");
+    }
+    else if(a!=0){
+        x = -b/a;
+        System.out.printf("He phuong trinh co nghiem duy nhat la: %f",x);
+    }
+    }
 }
+
